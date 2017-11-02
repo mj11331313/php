fc8ebe786a9d9059a1d8fd5eb728bfc29017476a
mj1331313 <author@2238178090@qq.com>
2017-11-02 22:43:25 +0800

    test

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+		echo "hello,I'm Cynthia";
+		#这是一个单行注释;
+		$color = "red";//定义变量并初始化
+		echo "<br>the color is ".$color;//.表示拼接
+	?>
+</body>
+</html>
