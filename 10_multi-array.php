311fbf2fed070ae065d4cafdc127d745a6f836e0
mj1331313 <author@2238178090@qq.com>
2017-11-04 22:57:28 +0800

    多维数组

--- 10_multi-array.php
+++ 10_multi-array.php
@@ -8,7 +8,26 @@
 </head>
 <body>
     <?php 
-        
+        //定义一个4行3列的数组：
+        $cars=array(
+            array('BMW',13,24),
+            array('Volvo',23,15),
+            array('Land Rover',20,23),
+            array('Saab',19,22)
+        );
+        //获取数组值：
+        echo $cars[0][0].'--库存: '.$cars[0][1].' 销量: '.$cars[0][2];
+        echo '<br>';
+        echo $cars[1][0].'--库存: '.$cars[1][1].' 销量: '.$cars[1][2];
+        echo "<br>";
+
+        //循环输出数组值：
+        for($row=0;$row<4;$row++){
+            for($col=0;$col<3;$col++){
+                echo $cars[$row][$col]." ";
+            };
+            echo "<br>";
+        }
     ?>
 </body>
 </html>
